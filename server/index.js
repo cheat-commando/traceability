d7ece434aafced0e7f15b5df1b6a99be7b978dd6
Carston Work <carston.work@gmail.com>
2021-08-31 16:12:07 -0600

    TRYING AGAIN

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
-const Rollbar = require('rollbar')
+const Rollbar = require('rollbar');
+const cors = require('cors');
+
 
 let rollbar = new Rollbar({
     accessToken: 'eac284207de144c2a6e4fff3bb06e11e',
@@ -8,13 +10,16 @@ let rollbar = new Rollbar({
     captureUnhandledRejections: true
 })
 
-let currentId = 1
+let currentId = 1;
 const peoplesInterest = []
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
-app.use('/styles', express.static('../client/styles.css'));
+app.get('/styles', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/styles.css'))
+})
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/index.html'))
@@ -22,11 +27,12 @@ app.get('/', (req, res) => {
 });
 
 function makeNewEntry(req, res) {
+    console.log(req.body)
     const { favClass, favGame, favColor } = req.body
-    if (favClass.toLowerCase() === 'math') {
+    if (favClass === 'math') {
         rollbar.critical('User thinks math is cool')
     }
-    if (favColor.toLowerCase() === 'green') {
+    if (favColor === 'green') {
         rollbar.warning('User has no artistic side')
     }
     const newEntry = {
@@ -49,18 +55,17 @@ function makeNewEntry(req, res) {
     }
 }
 
-app.post('/newInfo', () => {
+app.post('/newInfo', (req, res) => {
     try {
-        makeNewEntry
+        makeNewEntry(req, res)
     } catch (error) {
         rollbar.error(error)
     }
 })
 
-app.get('/healthCheck/:health', (req, res) => {
-    if (req.params.health === 20) {
-        rollbar.error('User has created an OP character')
-    }
+app.get('/healthCheck', (req, res) => {
+    console.log("op char")
+    rollbar.error('User has created an OP character')
     res.status(200)
 })
 
