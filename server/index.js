dbe6c3701b51975608130a6e0b0a0c422f92533e
Carston Work <carston.work@gmail.com>
2021-08-31 15:34:06 -0600

    Added health feature and changed favorites

--- server/index.js
+++ server/index.js
@@ -21,15 +21,33 @@ app.get('/', (req, res) => {
     rollbar.info('html file served successfully', {author: 'Carston'})
 });
 
-// function makeNewEntry(req, res) {
-//     const { favHero, favAnime, favColor } = req.body
-//     const newEntry = {
-//         id: currentId,favHero, favAnime, favColor
-//     }
-//     newEntry++
-//     peoplesInterest.push(newEntry)
-//     res.status(200).send(newEntry)
-// }
+function makeNewEntry(req, res) {
+    const { favClass, favGame, favColor } = req.body
+    if (favClass.toLowerCase() === 'math') {
+        rollbar.critical('User thinks math is cool')
+    }
+    if (favColor.toLowerCase() === 'green') {
+        rollbar.warning('User has no artistic side')
+    }
+    const newEntry = {
+        id: currentId,favClass, favGame, favColor
+    }
+    let isThereNull = false
+    for (piece in newEntry) {
+        if (piece === 'none') {
+            isThereNull = true
+        } 
+    } 
+    if (!isThereNull) {
+        newEntry++
+        peoplesInterest.push(newEntry)
+        rollbar.info('User successfully entered their favorites', {faveHero, faveAnime, favColor})
+        res.status(200).send(newEntry)
+    } else {
+        rollbar.error('User left a field blank')
+        res.status(406)
+    }
+}
 
 app.post('/newInfo', () => {
     try {
@@ -37,6 +55,13 @@ app.post('/newInfo', () => {
     } catch (error) {
         rollbar.error(error)
     }
+})
+
+app.get('/healthCheck/:health', (req, res) => {
+    if (req.params.health === 20) {
+        rollbar.error('User has created an OP character')
+    }
+    res.status(200)
 })
 
 const port = process.env.PORT || 4005;
